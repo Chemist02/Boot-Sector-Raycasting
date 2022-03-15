ce28b7ebd82950aee05dbe607af4a120e43af8c8
Griffin Bates <griffin0042@gmail.com>
2022-03-15 05:04:30 -0400

    Added sin approximation

--- Boot-Sector-Raycasting.asm
+++ Boot-Sector-Raycasting.asm
@@ -23,7 +23,7 @@ TICK_COUNTER	equ 0x046C
 ; Translational speed of player (again, remember that this is multiplied by 100 relative to our actual map).
 TRANSL_SPD	equ 50
 ; Angular speed of player (in degrees per delayTicks); not multiplied by anything.
-ANGULAR_SPD		equ 1
+ANGULAR_SPD	equ 1
 ; Screen dimensions.
 SCREEN_WIDTH	equ 80
 SCREEN_HEIGHT	equ 25
@@ -36,6 +36,9 @@ xPos:		dw 0
 yPos:		dw 0
 ; Player rotation in degrees.
 rot:		dw 0
+; Let 1000sin(x) be expressed as 1000sin(x) = mx + b (approx.), where m is slope and b is y intercept.
+SIN_SLOPES:	dw 17, 16, 13, 10, 6, 2, 0
+SIN_INTERCEPTS:	dw 0, 18, 109, 257, 506, 815, 1000
 
 ; ------------- START GAME -------------
 start:
@@ -54,12 +57,15 @@ start:
 ; ------------- FUNCTIONS -------------
 ; Prints contents of ax register to top of screen in hexadecimal.
 printWord:
+	push bp 
+	mov bp, sp
+
 	push ax
 	push cx
 	push dx
 	push di
 	; A word consists of 4 hex values.
-	mov di, 0
+	xor di, di
 	mov cx, 4
 	mov dx, ax
 	printWordLoop:
@@ -75,8 +81,107 @@ printWord:
 	pop dx
 	pop cx
 	pop ax
+
+	mov sp, bp
+	pop bp
 	ret
 
+; Returns index in board array given x and y coordinates (column, row), where column number is pushed onto stack before
+; row index. 
+getBoardIndex:
+	push bp 
+	mov bp, sp
+
+	push bx
+
+	; index = row num * screen width + col num.
+	mov bx, SCREEN_WIDTH
+	mov ax, [bp + 8]
+	mul bx
+	add ax, [bp + 6]
+	
+	pop bx
+
+	mov sp, bp
+	pop bp
+	ret
+
+; Returns sin(x) multiplied by 100 in ax, where x is the top most member of the stack and must be positive.
+sin:
+	push bp
+	mov bp, sp
+	
+	push bx
+	push dx
+	push si
+
+	; Get angle from stack.
+	mov ax, [bp + 4]
+	
+	; Get angle mod 180.
+	xor dx, dx
+	mov bx, 180
+	div bx
+	; Remainder (normalized angle) is now in dx.
+
+	; Ensure angle is between 0 and 90
+	mov bx, 90
+	cmp dx, bx
+	; If angle is less than 90, we're good.
+	jl dontMakeLessThan90
+	; Otherwise, subtract it from 180.
+	mov ax, 180
+	sub ax, dx
+	mov dx, ax
+	; ax now contains our value for x, between 0 and 90.
+	dontMakeLessThan90:
+	mov ax, dx
+
+	; Find linear approximation of 1000sin(x).
+	; We'll need our angle back later.
+	push ax
+	; Find which linear coefficients to use.
+	xor dx, dx
+	mov bx, 15
+	div bx
+	shl ax, 1
+	mov si, ax
+	mov bx, [SIN_SLOPES + si]
+	mov si, [SIN_INTERCEPTS + si]
+	; Get angle back and find 1000(sin(ax))
+	pop ax
+	mul bx
+	add ax, si
+
+	; Divide ax by 10.
+	mov bx, 10
+	div bx
+
+	; Determine if we need to negate and if so, do it.
+	xor dx, dx
+	mov si, ax
+	mov ax, [bp + 4]
+	mov bx, 360
+	div bx
+	; If normalized angle is greater than or equal to 180, negate.
+	shr bx, 1
+	cmp dx, bx
+	jl doNotNegateSin
+	; Negate (2s complement).
+	not si
+	inc si
+
+	doNotNegateSin:
+	; Move final answer into position.
+	mov ax, si
+
+	pop si
+	pop dx
+	pop bx
+
+	mov sp, bp
+	pop bp
+	ret
 
 ; ------------- GAME LOOP -------------
 gameLoop:
@@ -87,13 +192,8 @@ gameLoop:
 	mov cx, 80 * 25
 	; Repeatedly black out characters indexed with di for all characters in video memory (i.e. es:di).
 	rep stosw
-	
-	;xor di, di
-	mov ax, 0x0F42
-	call printWord
-	;mov [es:di], ax
-	;stosw
 
+	
 	; Apply delay to slow game speed (i.e. arcsonic).
 	mov ax, [TICK_COUNTER]
 	add ax, [delayTicks]
